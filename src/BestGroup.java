b89fec792b7ca881f063a698961596625dba734a
BojanTrboljevac <trboja@gmail.com>
2021-08-05 09:54:51 -0400

    YAYYYYYY

--- src/BestGroup.java
+++ src/BestGroup.java
@@ -1,9 +1,9 @@
 public class BestGroup {
 
-    public static void main(String[] args) {
+        //System.out.println("If you can see this, then it should be working");
+        //System.out.println("It is working !!! Yayyyyyyyyy !!!!!!");
+        // YAYYYYYYY
 
-        System.out.println("If you can see this, then it should be working");
-        System.out.println("It is working !!! Yayyyyyyyyy !!!!!!");
 
     }
 
